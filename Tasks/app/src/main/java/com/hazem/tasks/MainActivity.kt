85d3d95012a32cd48034845c91ced313a8361cfb
hazemkhaled241 <84819643+hazemkhaled241@users.noreply.github.com>
2022-08-11 20:23:57 +0200

    task three

--- Tasks/app/src/main/java/com/hazem/tasks/MainActivity.kt
+++ Tasks/app/src/main/java/com/hazem/tasks/MainActivity.kt
@@ -3,24 +3,20 @@ package com.hazem.tasks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RadioButton
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_toast.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    btn_order.setOnClickListener {
-        val checkButtonID= radioGroup.checkedRadioButtonId
-        val checkedButtonView=findViewById<RadioButton>(checkButtonID)
-        val cheese=cb_cheese.isChecked
-        val onions=cb_onions.isChecked
-        val salad=cb_salad.isChecked
-        val fullOrder="You ordered a burger with: \n" +
-                "${checkedButtonView.text}" +
-                (if(cheese)"\ncheese" else "")+
-                (if(onions)"\nonions" else "")+
-                (if(salad)"\nsalad" else "")
-        tv_full_order.text=fullOrder.toString()
-    }
+       btn_show.setOnClickListener {
+           Toast(this).apply {
+               duration=Toast.LENGTH_SHORT
+               view=layoutInflater.inflate(R.layout.custom_toast,root_of_custom)
+               show()
+           }
+       }
     }
 }
